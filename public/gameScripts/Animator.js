ced53d6450ba84ec4ee46c39c9876b1d7b47a52c
José Martínez <wintryart@gmail.com>
2024-11-22 11:39:15 -0600

    animaciones2

--- public/gameScripts/Animator.js
+++ public/gameScripts/Animator.js
@@ -10,6 +10,19 @@ export class Animator {
 
         this.mixer = mixer;
         this.animations = animations;
+        this.currentState = 'idle';
+
+        this.isJumping = false;
+
+
+        this.mixer.addEventListener('finished', (event) => {
+            if (this.isJumping) {
+                console.log('Salto terminado', event);
+                this.isJumping = false;
+                this.currentState = 'idle';
+            }
+
+        });
     }
 
     update() {
@@ -19,7 +32,10 @@ export class Animator {
     }
 
     jump(animationIndex) {
-        this.mixer.stopAllAction();
+        if (this.currentState == 'jump') {
+            return;
+        }
+        this.currentState = 'jump';
         this.action = this.mixer.clipAction(this.animations[animationIndex]);
         this.action.setLoop(THREE.LoopOnce);
         // action.clampWhenFinished = true;
@@ -28,11 +44,43 @@ export class Animator {
     }
 
     move(animationIndex) {
-        this.mixer.stopAllAction();
+        console.log('intenado entrar:', this.currentState);
+        if (this.currentState == 'move' || this.currentState == 'jump') {
+            return;
+        }
+        console.log('entró--------------------------------------:');
+        this.currentState = 'move';
         this.action = this.mixer.clipAction(this.animations[animationIndex]);
         this.action.setLoop(THREE.LoopRepeat);
         // action.clampWhenFinished = true;
         // action.enable = true;
         this.action.play();
     }
+
+    setState(state) {
+
+        console.log('estado:', this.currentState);
+
+        if (this.isJumping) {
+            return;
+        }
+
+        if (state == 'jump') {
+            this.mixer.stopAllAction();
+            this.isJumping = true;
+            this.jump(0);
+            return;
+
+        }
+        if (state == 'move') {
+            this.move(1);
+            return;
+        }
+
+        if (state == 'idle') {
+            this.mixer.stopAllAction();
+            this.currentState = 'idle';
+        }
+    }
+
 }
